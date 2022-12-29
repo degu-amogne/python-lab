7ff7bff94d93accb1d17a65b7c9171b1148ddd71
Ermiyas <ermiyasyamlak3030@gmial.com>
2022-12-29 16:22:03 +0300

    lab.py file created by ermi

---
+++ lab.py
@@ -0,0 +1,22 @@
+def say_hello():
+    print("hello there")
+say_hello()
+#this is comment
+'''this is doc string'''
+height = 1.9 
+wieght = 100
+fruit,animal = "banana" , "Dog"
+x=y=z="orange"
+print(type(x))
+print(len(y))
+print (animal)
+#name = input()
+"""a= input()
+b= input ()
+sum = int(a) + int(b)
+print (sum)"""
+name = input("your name")
+age = input("your age")
+numbers = input("your id")
+print (f"flout {numbers}")
+print(f"Hello {name} your age is {age}")
